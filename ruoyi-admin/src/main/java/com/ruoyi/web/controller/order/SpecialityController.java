576df0f2e3019daa4e9d8593754d6a1724fbae39
shiwei1 <shiwei1@ysstech.com>
2021-04-23 17:48:06 +0800

    [add-新增功能] 功能描述: 影响范围: StoryID :
    [modify-问题修复]
    问题原因:
    解决方法:
    影响范围:
    BugID   :
    
    [remove-代码清理]
    清理原因:
    影响范围:

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/order/SpecialityController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/order/SpecialityController.java
@@ -11,11 +11,18 @@ package com.ruoyi.web.controller.order;/****************************************
  ********************************************************/
 
 
+import com.ruoyi.system.domain.Food;
+import com.ruoyi.system.service.impl.FoodServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -28,15 +35,31 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class SpecialityController {
 
+    @Autowired
+    private FoodServiceImpl foodService;
 
     @PostMapping("/foodInfo")
     @ResponseBody
-    private String  speciality(@RequestParam String foodid){
+    private String speciality(@RequestParam String foodId) {
         return "/system/web/FoodInfo";
     }
 
+    @GetMapping("/speciality")
+    private String foodType(@RequestParam String foodType, Model model) {
+
+        List<Food> list = new ArrayList<>();
+
+        Food food = foodService.selectFoodById(Long.valueOf(foodType));
+
+        list.add(food);
+
+        model.addAttribute(list);
+
+        return "orderIndex";
+    }
+
     @GetMapping("/system/web/FoodInfo")
-    private String  foodInfo(){
+    private String foodInfo() {
         return "/system/web/FoodInfo";
     }
 
